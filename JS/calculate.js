46e81f668ecd01639b73d25f7e7fe9bfd680b6b3
polon59 <przemyslaw.polczak@gmail.com>
2018-09-21 00:27:23 +0200

    Add file 'calculate.js'

---
+++ JS/calculate.js
@@ -0,0 +1,45 @@
+var makarov, ak47 = 0;
+var weaponNames = [["makarov", 3], ["ak47",5], ["beretta",2]];
+var ammoAmmount = []; 
+var sum;
+createAmmoAmmountDict();
+
+
+function createAmmoAmmountDict() {
+    for (var i = 0; i<weaponNames.length; i++){
+        var weapon = {name: weaponNames[i][0], cost: weaponNames[i][1], shots: 0};
+        ammoAmmount.push(weapon);
+    }
+}
+
+function changeShotsNumber(weaponName, action) {
+    var elementID = weaponName + "ShotsNumber";
+    var numberOfWeaponShots;
+
+    ammoAmmount.forEach(weapon => {
+        if (weapon.name == weaponName) {
+
+            if (action == "add") {
+                weapon.shots ++;
+            }
+            else{
+                if (weapon.shots > 0) {
+                    weapon.shots--;
+                }
+            } 
+            numberOfWeaponShots = weapon.shots;
+        }   
+    });
+    document.getElementById(elementID).innerHTML = numberOfWeaponShots;
+}
+
+
+function calculate() {
+    sum = 0;
+    ammoAmmount.forEach(weapon => {
+        sum += weapon.shots * weapon.cost;
+    });
+    document.getElementById("sum").innerHTML = sum;
+}
+
+
