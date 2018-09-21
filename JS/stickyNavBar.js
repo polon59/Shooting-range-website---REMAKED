a7f051af2858306132c16791279d8bce2675b914
polon59 <przemyslaw.polczak@gmail.com>
2018-09-21 17:03:52 +0200

    Change spacing between horizontal and vertical navbars

--- JS/stickyNavBar.js
+++ JS/stickyNavBar.js
@@ -6,7 +6,7 @@ var stickyVER = navbarVer.offsetTop;
 var stickyHOR = navbarHor.offsetTop;
 
 function stickNavbar() {
-  if (window.pageYOffset >= stickyVER) {
+  if (window.pageYOffset >= stickyVER-85) {
     navbarVer.classList.add("stickyVer");
   } else {
     navbarVer.classList.remove("stickyVer");
@@ -14,7 +14,9 @@ function stickNavbar() {
 
   if (window.pageYOffset >= stickyHOR) {
     navbarHor.classList.add("stickyHor");
+    // navbarVer.classList.add("stickyVer");
   } else {
     navbarHor.classList.remove("stickyHor");
+    // navbarVer.classList.remove("stickyVer");
   }
 }
