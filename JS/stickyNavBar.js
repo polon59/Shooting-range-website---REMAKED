3db7f6120d4f9ca6de72aa91b8dd03c31cba9f6d
polon59 <przemyslaw.polczak@gmail.com>
2018-09-21 20:56:20 +0200

    Change vertical navbar properties to avoid sticky effect

--- JS/stickyNavBar.js
+++ JS/stickyNavBar.js
@@ -6,11 +6,11 @@ var stickyVER = navbarVer.offsetTop;
 var stickyHOR = navbarHor.offsetTop;
 
 function stickNavbar() {
-  if (window.pageYOffset >= stickyVER-85) {
-    navbarVer.classList.add("stickyVer");
-  } else {
-    navbarVer.classList.remove("stickyVer");
-  }
+  // if (window.pageYOffset >= stickyVER-85) {
+  //   navbarVer.classList.add("stickyVer");
+  // } else {
+  //   navbarVer.classList.remove("stickyVer");
+  // }
 
   if (window.pageYOffset >= stickyHOR) {
     navbarHor.classList.add("stickyHor");
