53e1cbf391cb472f055e2a0f4bd4d670e3d0f0c7
polon59 <przemyslaw.polczak@gmail.com>
2018-09-23 02:12:11 +0200

    Add thompson carabine to arsenal page

--- JS/calculate.js
+++ JS/calculate.js
@@ -1,4 +1,4 @@
-var weaponNames = [["ak74", 3], ["ak47",5], ["ppsz",2]];
+var weaponNames = [["ak74", 3], ["ak47",5], ["ppsz",2], ["thompson",2.5]];
 var ammoAmmount = []; 
 var sum;
 createAmmoAmmountDict();
